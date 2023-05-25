fa11217e22c207aeb3baf55a617745395fb594a6
Otto <justigo86@gmail.com>
2023-05-24 22:36:49 -0400

    updated Categories for Toolkit

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,19 +1,39 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+//no longer needed with Toolkit
+// import { CATEGORIES_ACTION_TYPES } from './category.types';
+
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
 };
 
-export const categoriesReducer = (
-  state = CATEGORIES_INITIAL_STATE,
-  action = {}
-) => {
-  const { type, payload } = action;
+//similar structure to User Toolkit reducer
+export const categoriesReducerSlice = createSlice({
+  name: "categories",
+  initialState: CATEGORIES_INITIAL_STATE,
+  reducers: {
+    setCategories(state, action) {
+      state.categories = action.payload;
+    },
+  },
+});
 
-  switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
-    default:
-      return state;
-  }
-};
+//no longer needed with Toolkit
+// export const categoriesReducer = (
+//   state = CATEGORIES_INITIAL_STATE,
+//   action = {}
+// ) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+//       return { ...state, categories: payload };
+//     default:
+//       return state;
+//   }
+// };
+
+export const { setCategories } = categoriesReducerSlice.actions;
+//actions is property of object - contains all ACTIONS from within slice reducer
+
+export const categoriesReducer = categoriesReducerSlice.reducer;
